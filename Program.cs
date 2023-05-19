57fe17892a1a96e7539103ae75e6498100d64559
javier gimenez <javierzlafe@gmail.com>
2023-05-19 10:46:43 -0300

    finish date class

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using ConceptsPOO;
+
+Console.WriteLine("Hello, World!");
+
+
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(22, 2, 15));
+	Console.WriteLine(new Date(2022, 11, 31));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
